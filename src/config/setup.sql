08d9244273cd50f29bf0bd6367c7a69375529b95
Nguyen Cao Thiem <1851061743@e.tlu.edu.vn>
2022-10-05 01:15:30 +0700

    Init sql.setup

--- src/config/setup.sql
+++ src/config/setup.sql
@@ -1,91 +1,131 @@
 
 -- SINH VIEN
+DROP TABLE IF EXISTS SINHVIEN;
 CREATE TABLE SINHVIEN (
     MaSV            INT,
     Hoten           VARCHAR(255),
-    Ngaysinh        VARCHAR(255),
+    Ngaysinh        DATE,
     Gioitinh        VARCHAR(255),
     MaThietbi       VARCHAR(255),
-    CONSTRAINT PK_SINHVIEN	PRIMARY KEY(MaSV) 
+    MaFCM           VARCHAR(255),
+    PRIMARY KEY(MaSV)
 );
 
 -- KHOA
+DROP TABLE IF EXISTS KHOA;
 CREATE TABLE KHOA (
     MaKhoa          INT,
     TenKhoa         VARCHAR(255),
-    CONSTRAINT PK_KHOA	PRIMARY KEY(MaKhoa) 
+    PRIMARY KEY(MaKhoa) 
 );
 
 -- PHONGHOC
+DROP TABLE IF EXISTS PHONGHOC;
 CREATE TABLE PHONGHOC (
     MaPH            INT,
     TenPH           VARCHAR(255),
     Diachi          VARCHAR(255),
-    CONSTRAINT PK_PHONGHOC PRIMARY KEY(MaPH) 
+    PRIMARY KEY(MaPH) 
 );
 
 -- CAHOC
+DROP TABLE IF EXISTS CAHOC;
 CREATE TABLE CAHOC (
     MaCH            INT,
     GioBatdau       VARCHAR(255),
     GioKetthuc      VARCHAR(255),
-    CONSTRAINT PK_CAHOC PRIMARY KEY(MaCH) 
+    PRIMARY KEY(MaCH) 
 );
 
 -- GIANGVIEN
+DROP TABLE IF EXISTS GIANGVIEN;
 CREATE TABLE GIANGVIEN (
     MaGV            INT,
+    MaKhoa          INT,
     Hoten           VARCHAR(255),
-    MaKhoa          VARCHAR(255),
+    Ngaysinh        DATE,
     Gioitinh        VARCHAR(255),
     MaThietbi       VARCHAR(255),
-    CONSTRAINT PK_GIANGVIEN PRIMARY KEY(MaGV) 
+    MaFCM           VARCHAR(255),
+    PRIMARY KEY(MaGV),
+    FOREIGN KEY (MaKhoa) REFERENCES KHOA(MaKhoa)
 );
 
 -- MONHOC
+DROP TABLE IF EXISTS MONHOC;
 CREATE TABLE MONHOC (
     MaMH            INT,
     TenMonhoc       VARCHAR(255),
-    Tongsotiet      VARCHAR(255),
-    Tietlythuyet    VARCHAR(255),
-    Tietthuchanh    VARCHAR(255),
-    CONSTRAINT PK_MaMH PRIMARY KEY(MaMH) 
+    Tongsotiet      INT,
+    Tietlythuyet    INT,
+    Tietthuchanh    INT,
+    PRIMARY KEY(MaMH) 
+);
+
+-- LOPMONHOC
+DROP TABLE IF EXISTS LOPMONHOC;
+CREATE TABLE LOPMONHOC (
+    MaLopMonhoc     INT,
+    MaGV            INT,
+    TenLopmonhoc    VARCHAR(255),
+    PRIMARY KEY(MaLopMonhoc),
+    FOREIGN KEY (MaGV) REFERENCES GIANGVIEN(MaGV)
+);
+
+
+-- GV_DIEMDANH_SV
+DROP TABLE IF EXISTS GV_DIEMDANH_SV;
+CREATE TABLE GV_DIEMDANH_SV (
+    MaDiemDanh      INT,
+    MaGV            INT,
+    MaLopMonhoc     INT,
+    Ngaydiemdanh    Date,
+    Thoidiembatdau  Date,
+    PRIMARY KEY(MaDiemDanh),
+    FOREIGN KEY (MaGV) REFERENCES GIANGVIEN(MaGV),
+    FOREIGN KEY (MaLopMonhoc) REFERENCES LOPMONHOC(MaLopMonhoc)
 );
 
 -- MONHOC_LOPMONHOC
+DROP TABLE IF EXISTS MONHOC_LOPMONHOC;
 CREATE TABLE MONHOC_LOPMONHOC (
     MaLopMonhoc     INT,
     MaMH            INT,
     Namhoc          VARCHAR(255),
-    Hocky           VARCHAR(255), 
-);
-
--- LOPMONHOC
-CREATE TABLE LOPMONHOC (
-    MaLopMonhoc     INT,
-    TenLopmonhoc    INT,
-    MaGV            VARCHAR(255),
-    CONSTRAINT PK_MaLopMonhoc PRIMARY KEY(MaLopMonhoc) 
+    Hocky           VARCHAR(255),
+    FOREIGN KEY (MaLopMonhoc) REFERENCES LOPMONHOC(MaLopMonhoc),
+    FOREIGN KEY (MaMH) REFERENCES MONHOC(MaMH)
 );
 
 -- CA_PHONG_LOPMONHOC
+DROP TABLE IF EXISTS CA_PHONG_LOPMONHOC;
 CREATE TABLE CA_PHONG_LOPMONHOC (
     MaCH            INT,
     MaPH            INT,
     MaLopMonhoc     INT,
-    Thu             VARCHAR(255)
+    Thu             VARCHAR(255),
+    FOREIGN KEY (MaCH) REFERENCES CAHOC(MaCH),
+    FOREIGN KEY (MaPH) REFERENCES PHONGHOC(MaPH),
+    FOREIGN KEY (MaLopMonhoc) REFERENCES LOPMONHOC(MaLopMonhoc)
 );
 
 -- CHITIETDD
-CREATE TABLE CHITIETDD {
+DROP TABLE IF EXISTS CHITIETDD;
+CREATE TABLE CHITIETDD (
     MaDiemDanh      INT,
     MaSV            INT,
-    Lydo            VARCHAR(255)   
-}
+    Thoigiandiemdanh VARCHAR(255),
+    Lydo            VARCHAR(255),
+    FOREIGN KEY (MaDiemDanh) REFERENCES GV_DIEMDANH_SV(MaDiemDanh),
+    FOREIGN KEY (MaSV) REFERENCES SINHVIEN(MaSV)
+);
 
--- GV_DIEMDANH_SV
-CREATE TABLE GV_DIEMDANH_SV {
-    MaDD            INT,
+-- SINHVIEN_HOC_LOPMONHOC
+DROP TABLE IF EXISTS SINHVIEN_HOC_LOPMONHOC;
+CREATE TABLE SINHVIEN_HOC_LOPMONHOC (
+    MaLopMonhoc     INT,
     MaSV            INT,
-    Lydo            VARCHAR(255)   
-}
+    Kichhoat        BOOLEAN,
+    FOREIGN KEY (MaLopMonhoc) REFERENCES LOPMONHOC(MaLopMonhoc),
+    FOREIGN KEY (MaSV) REFERENCES SINHVIEN(MaSV)
+);
